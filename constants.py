c9a09d1565bf77f6a4a500ee1795cc4290d3a2db
Unknown <jeremy.tudisco@gmail.com>
2018-07-09 00:49:59 -0400

    API requests to use HTTPS
    Not sure if each http redirect was a freebie or if it counted towards BGG throttling me, but hey, at least we'll start doing the right thing.

--- constants.py
+++ constants.py
@@ -58,9 +58,9 @@ VERSION_URL = "https://boardgamegeek.com/item/boardgameversion/{id}/xxx"
 VERSION_EDIT_URL = "https://boardgamegeek.com/item/correction/boardgameversion/{id}"
 GAME_URL    = "https://boardgamegeek.com/boardgame/{id}"
 GAME_VERSIONS_URL = "https://boardgamegeek.com/boardgame/{id}/xxx/versions"
-API_COLL_URL    = 'http://www.boardgamegeek.com/xmlapi2/collection?username={id}&own=1&version=1&stats=1'
-API_GAME_URL    = 'http://www.boardgamegeek.com/xmlapi2/thing?id={ids}&stats=1&versions=1'
-OLD_API_GAME_URL    = 'http://www.boardgamegeek.com/xmlapi/boardgame/{ids}'
+API_COLL_URL    = 'https://www.boardgamegeek.com/xmlapi2/collection?username={id}&own=1&version=1&stats=1'
+API_GAME_URL    = 'https://www.boardgamegeek.com/xmlapi2/thing?id={ids}&stats=1&versions=1'
+OLD_API_GAME_URL    = 'https://www.boardgamegeek.com/xmlapi/boardgame/{ids}'
 
 
 CACHE_DIR = "__cache__"
